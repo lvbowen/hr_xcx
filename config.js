ee16c0f27331ec0804786b6d540e0e3065c2eebf
Bovin Lv <1010606892@qq.com>
2018-11-02 14:40:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let config = {
-  host:"https://aijuhr.com/hrm",
+  host:"https://aijutong.com/hrm",     //测试
   // appId:"wx59ac83234ec651e4",  
   // appSecret:'5e73f0ddb0ce25cdd743a2be892b77d7',
   lversion:"1.0.1",
